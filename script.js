ed4d72c230df7e43d65acaf0df8c39a13177dc94
asolermaria <asolermaria@gmail.com>
2026-03-10 20:55:06 +0100

    Ejercicio 1 done

---
+++ script.js
@@ -0,0 +1,28 @@
+// Ejercicio 1
+
+const lista = document.getElementById("lista")
+
+// Primer elemento (Forma moderna, usando innerHTML)
+lista.innerHTML += "<li>Soy el primer elemento de la lista</li>"
+
+// Segundo elemento (Forma clásica, usando createElement(), textContent y appendChild())
+const li2 = document.createElement("li")
+li2.textContent = "Soy el segundo elemento de la lista"
+
+const atributo2 = document.createAttribute("class")
+atributo2.value = "elemento-lista"
+li2.setAttributeNode(atributo2)
+
+lista.appendChild(li2)
+
+// Tercer elemento (Forma clásica, usando createElement(), createTextNode() y appendChild())
+const li3 = document.createElement("li")
+const texto = document.createTextNode("Soy el tercer elemento de la lista")
+li3.appendChild(texto)
+
+const atributo3 = document.createAttribute("class")
+atributo3.value = "elemento-lista"
+li3.setAttributeNode(atributo3)
+
+lista.appendChild(li3)
+
