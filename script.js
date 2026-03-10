1bb268bdc9b469aae3741cd65e72266f35aff4f0
asolermaria <asolermaria@gmail.com>
2026-03-10 22:15:57 +0100

    100% done

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 // Ejercicio 1
 
-const lista = document.getElementById("lista")
+const lista = document.getElementById("lista");
 
 // Primer elemento (Forma moderna, usando innerHTML)
-lista.innerHTML += "<li>Soy el primer elemento de la lista</li>"
+lista.innerHTML +=
+  "<li class='elemento-lista'>Soy el primer elemento de la lista</li>";
 
-// Segundo elemento (Forma clásica, usando createElement(), textContent y appendChild())
-const li2 = document.createElement("li")
-li2.textContent = "Soy el segundo elemento de la lista"
+// Segundo elemento (Forma clásica, usando textContent)
+const li2 = document.createElement("li");
+li2.textContent = "Soy el segundo elemento de la lista";
 
-const atributo2 = document.createAttribute("class")
-atributo2.value = "elemento-lista"
-li2.setAttributeNode(atributo2)
+const atributo2 = document.createAttribute("class");
+atributo2.value = "elemento-lista";
+li2.setAttributeNode(atributo2);
 
-lista.appendChild(li2)
+lista.appendChild(li2);
 
-// Tercer elemento (Forma clásica, usando createElement(), createTextNode() y appendChild())
-const li3 = document.createElement("li")
-const texto = document.createTextNode("Soy el tercer elemento de la lista")
-li3.appendChild(texto)
+// Tercer elemento (Forma clásica, usando createTextNode())
+const li3 = document.createElement("li");
+const texto = document.createTextNode("Soy el tercer elemento de la lista");
+li3.appendChild(texto);
 
-const atributo3 = document.createAttribute("class")
-atributo3.value = "elemento-lista"
-li3.setAttributeNode(atributo3)
+const atributo3 = document.createAttribute("class");
+atributo3.value = "elemento-lista";
+li3.setAttributeNode(atributo3);
 
-lista.appendChild(li3)
+lista.appendChild(li3);
 
+// Ejercicio 2
+
+const labores = ["comprar","barrer","alimentar gato","colada"]
+
+for (const labor of labores){
+    const liLabor = document.createElement("li")
+    liLabor.textContent = labor
+    lista.appendChild(liLabor)
+}
